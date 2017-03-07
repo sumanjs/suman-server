bb56d99364f0e1d4cd632c17c25ed46f20633feb
Olegzandr <alex@oresoftware.com>
2017-03-07 10:39:48 -0800

    extro

--- roodles.conf.js
+++ roodles.conf.js
@@ -1,6 +1,28 @@
 const path = require('path');
 
+// module.exports = {
+//   exec: 'bin/www.js',
+//   exclude: [path.resolve(__dirname + '/node_modules/.*'), '.git', 'test', 'public','.idea']
+// };
+
+
 module.exports = {
-  exec: 'bin/www.js',
-  exclude: [path.resolve(__dirname + '/node_modules/.*'), '.git', 'test', 'public','.idea']
+ exec: 'bin/www.js',  // any binary file or a file with a hashbang
+ include: [__dirname],
+ exclude: [
+  'public',
+  '.git',
+  '.idea',
+  'package.json',
+  'node_modules',
+  '.*\.log',
+  '.*\.sh'
+ ],
+ signal: 'SIGINT',
+ restartUponChange: true,
+ restartUponAddition: false,
+ restartUponUnlink: false,
+ processLogPath: null,         // if desired, pass a relative or absolute path to log file
+ verbosity: 1,  // an integer {1,2,3}
+ processArgs: []
 };
